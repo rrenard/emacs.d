1a38f98c5526773a19f31d92c108ce0299ae4a0f
Richard Renard <rrenard@users.noreply.github.com>
2017-09-17 12:11:28 +0200

    updates on org config

--- orgmode.el
+++ orgmode.el
@@ -1,16 +1,27 @@
 
+
+
+;; see https://lists.gnu.org/archive/html/emacs-orgmode/2008-08/msg00106.html
+;;     https://lists.gnu.org/archive/html/emacs-orgmode/2008-08/msg00139.html
+;; Don't convert _ and ^ unless surrounded by braces
+;; #+OPTIONS:   ^:{} # in file
+;; or
+;; (setq org-export-with-sub-superscripts '{})
+
+
 (use-package org
   :ensure t
   :defer t
-  :bind ("C-c c" . org-capture)
-  :bind ("C-c l" . org-store-link)
-  :bind ("C-c a" . org-agenda)
+  :bind (("C-c c" . org-capture)
+         ("C-c l" . org-store-link)
+         ("C-c a" . org-agenda))
 
   :config
   (setq org-directory "~/org"
         org-agenda-files (list "~/org/notes.org"
                                "~/org/work.org")
-
+        ;; Don't convert _ and ^ unless surrounded by braces
+        org-export-with-sub-superscripts '{}
         org-default-notes-file (concat org-directory "/notes.org")
         org-capture-templates (quote (
                                       ("t" "Tasks" entry (file+headline "~/org/notes.org" "Tasks") "* TODO %?")
